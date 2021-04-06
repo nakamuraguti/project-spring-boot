94eeef8e8872ad200fded36940d4943804998640
Gustavo Nakamura <nakamuraguti@gmail.com>
2021-04-06 12:38:29 -0300

    Service Layer - Component Registration

---
+++ src/main/java/com/project/workshop/services/UserService.java
@@ -0,0 +1,27 @@
+package com.project.workshop.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.workshop.entities.User;
+import com.project.workshop.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
